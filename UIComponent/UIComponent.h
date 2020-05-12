fe5e411122a13a45a9a0d1a697db9f5dd5f08220
stNamco <lbnamco15@gmail.com>
2020-05-12 23:12:16 +0900

    added ui component target

---
+++ UIComponent/UIComponent.h
@@ -0,0 +1,19 @@
+//
+//  UIComponent.h
+//  UIComponent
+//
+//  Created by stnamco on 2020/05/12.
+//  Copyright © 2020 namco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIComponent.
+FOUNDATION_EXPORT double UIComponentVersionNumber;
+
+//! Project version string for UIComponent.
+FOUNDATION_EXPORT const unsigned char UIComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIComponent/PublicHeader.h>
+
+
